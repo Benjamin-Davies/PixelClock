7fe70097695c5b6d8c3d89b6f6deb84b21078060
Benjamin Davies <bentendavies@gmail.com>
2017-04-27 11:21:35 +1200

    Created first prototype

--- PixelClock/MainWindow.xaml.cs
+++ PixelClock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PixelClock
 {
@@ -20,9 +21,87 @@ namespace PixelClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SpriteManager sm;
+        private Image[] numbers;
+        private Image period, day;
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // events
+            SizeChanged += MainWindow_SizeChanged;
+            PreviewKeyDown += MainWindow_PreviewKeyDown;
+            timer = new DispatcherTimer(new TimeSpan(0, 0, 0, 0, 500), DispatcherPriority.Normal, new EventHandler(Timer_Elapsed), Dispatcher);
+
+            // pixelate not blur
+            RenderOptions.SetBitmapScalingMode(this, BitmapScalingMode.NearestNeighbor);
+
+            sm = new SpriteManager(Properties.Resources.sprites);
+
+            // init the numbers
+            numbers = new Image[8];
+            int k = 0; // index counter
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    var image = new Image();
+                    image.SetValue(Grid.ColumnProperty, j);
+                    image.SetValue(Grid.RowProperty, i);
+                    MainGrid.Children.Add(image);
+                    numbers[k] = image;
+                    k++;
+                }
+            }
+
+            // init period
+            period = new Image();
+            period.SetValue(Grid.ColumnProperty, 5);
+            period.SetValue(Grid.RowProperty, 0);
+            MainGrid.Children.Add(period);
+
+            // init days
+            day = new Image();
+            day.SetValue(Grid.ColumnProperty, 5);
+            day.SetValue(Grid.RowProperty, 1);
+            MainGrid.Children.Add(day);
+        }
+
+        private void Timer_Elapsed(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+
+            // numbers
+            string s = now.ToString("hhmmddMM");
+            for (int i = 0; i < 8; i++)
+            {
+                numbers[i].Source = sm.Numbers[int.Parse(s[i].ToString()) % 10, i % 4 > 1 ? 1 : 0];
+            }
+
+            // period am/pm
+            int periodNumber = now.Hour > 11 ? 1 : 0;
+            period.Source = sm.Periods[periodNumber];
+
+            // day of week
+            int dayNumber = (int)now.DayOfWeek;
+            day.Source = sm.Days[dayNumber];
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WindowStyle = WindowState == WindowState.Maximized ? WindowStyle.None : WindowStyle.SingleBorderWindow;
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    Close();
+                    break;
+            }
         }
     }
 }
---
+++ PixelClock/SpriteManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace PixelClock
+{
+    /// <summary>
+    /// Manages the sprite cropped bitmaps
+    /// </summary>
+    class SpriteManager
+    {
+        public BitmapSource Sheet;
+        public CroppedBitmap[,] Numbers;
+        public CroppedBitmap[] Periods;
+        public CroppedBitmap[] Days;
+
+        public SpriteManager(Bitmap bmp)
+        {
+            Sheet = Imaging.CreateBitmapSourceFromHBitmap(
+               bmp.GetHbitmap(),
+               IntPtr.Zero,
+               Int32Rect.Empty,
+               BitmapSizeOptions.FromEmptyOptions());
+
+            Numbers = new CroppedBitmap[10, 2];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 2; j++)
+                    Numbers[i, j] = new CroppedBitmap(Sheet, new Int32Rect(6 * i, 6 * j, 6, 6));
+
+            Periods = new CroppedBitmap[2];
+            for (int i = 0; i < 2; i++)
+                Periods[i] = new CroppedBitmap(Sheet, new Int32Rect(12 * i, 18, 12, 6));
+
+            Days = new CroppedBitmap[7];
+            for (int i = 0; i < 7; i++)
+                Days[i] = new CroppedBitmap(Sheet, new Int32Rect(12 * i, 12, 12, 6));
+        }
+    }
+}
